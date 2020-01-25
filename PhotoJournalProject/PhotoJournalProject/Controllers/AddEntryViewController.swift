bd84dba2c42cc0471c7643bfc2dfb1e2d5e7eab7
Gregory Keeley <gregorykeeley@pursuit.org>
2020-01-25 18:12:25 -0500

    - building view controllers (ViewController, AddJournalEntryController)

--- PhotoJournalProject/PhotoJournalProject/Controllers/AddEntryViewController.swift
+++ PhotoJournalProject/PhotoJournalProject/Controllers/AddEntryViewController.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
+import DataPersistence
 
 class AddEntryViewController: UIViewController {
 
+    @IBOutlet weak var entryTextField: UITextField!
     
-    
-    
+    private var newJournalEntry: Entry?
+    private var selectedImage: UIImage? {
+        didSet {
+            appendNewImageToCollection()
+        }
+    }
+    private let dataPersistence = DataPersistence<Entry>(filename: "entries.plist")
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+    private func appendNewImageToCollection() {
+        guard let image = selectedImage else {
+            print("Image is nil")
+            return
+        }
+        let size = UIScreen.main.bounds.size
+        let rect = AVMakeRect(aspectRatio: image.size, insideRect: CGRect(origin: CGPoint.zero, size: size))
+        let resizedImage = image.resizeImage(to: rect.size.width, height: rect.size.height)
+        guard let resizedImageData = resizedImage.jpegData(compressionQuality: 1.0) else {
+            return
+        }
+        let entryObject = Entry(imageData: resizedImageData, date: Date(), caption: entryTextField.text ?? "")
+        do {
+            try dataPersistence.createItem(entryObject)
+        } catch {
+            print("Error saving journal entry: \(error)")
+        }
+    }
 
 }
--- PhotoJournalProject/PhotoJournalProject/Controllers/ViewController.swift
+++ PhotoJournalProject/PhotoJournalProject/Controllers/ViewController.swift
@@ -7,17 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
+import DataPersistence
 
 class ViewController: UIViewController {
     
 //  cell reuseID: entryCell
+    @IBOutlet weak var collectionView: UICollectionView!
     
     
+    private var journalEntries = [Entry]()
+    private var imagePickerController = UIImagePickerController()
+
+    private var dataPersistence = DataPersistence<Entry>(filename: "entries.plist")
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    
 
 }
-
+extension UIImage {
+    func resizeImage(to width: CGFloat, height: CGFloat) -> UIImage {
+        let size = CGSize(width: width, height: height)
+        let renderer = UIGraphicsImageRenderer(size: size)
+        return renderer.image { (context) in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+}
