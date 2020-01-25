258f497537f6f3021fe64d9ee14daa43b3616989
Gregory Keeley <gregorykeeley@pursuit.org>
2020-01-25 16:24:20 -0500

    - Initiated project - Built basic UI framework

---
+++ PhotoJournalProject/PhotoJournalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoJournalProject
+//
+//  Created by Gregory Keeley on 1/25/20.
+//  Copyright © 2020 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
